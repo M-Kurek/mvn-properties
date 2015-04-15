05395b2b1551a03ce2a1c0f5421a50a7594c845b
M-Kurek <kurekmaciek@gmail.com>
2015-04-15 23:31:13 +0200

    init

---
+++ src/main/java/mk/abc/Hello.java
@@ -0,0 +1,15 @@
+package mk.abc;
+
+public class Hello {
+
+    private String message;
+
+    public Hello(String msg) {
+        message = msg;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+
+}
---
+++ src/test/java/mk/abc/HelloTest.java
@@ -0,0 +1,13 @@
+package mk.abc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class HelloTest {
+
+    @Test
+    public void testGetMessage() throws Exception {
+        //fail("NIY");
+    }
+}
